292148b98730f9b1a3654479a45105859605abfa
anwinsv <63356034+anwinsv@users.noreply.github.com>
2020-04-08 20:59:06 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,12 +3,12 @@ a,b=0,1
 count=0
 if n<=0:
   print("Enter a number greater than 0")
-elif n==1
+elif n==1:
   print("Fibonacci series upto",n,"term:")
   print(a)
-else
-  printf("Fibonacci series upto",n,"terms:")
-  while count<n
+else:
+  print("Fibonacci series upto",n,"terms:")
+  while count<n:
     print(a)
     c=a+b
     a=b

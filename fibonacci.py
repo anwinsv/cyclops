a4a85da0b2b7328526b6104ba57310271a38953a
anwinsv <63356034+anwinsv@users.noreply.github.com>
2020-04-08 20:49:22 +0530

    Created fibonacci.py
    Program to display the Fibonacci series up to a given limit.

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+n=int(input("Enter the limit:"))
+a,b=0,1
+count=0
+if n<=0:
+  print("Enter a number greater than 0")
+elif n==1
+  print("Fibonacci series upto",n,"term:")
+  print(a)
+else
+  printf("Fibonacci series upto",n,"terms:")
+  while count<n
+    print(a)
+    c=a+b
+    a=b
+    b=c
+    count+=1
